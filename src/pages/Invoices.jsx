048bc86f4fea5ddbe968b68e09f3dcdca33a1b53
isroilov-sardor <notfalse01@gmail.com>
2025-02-10 15:02:58 +0500

    done full mid

--- src/pages/Invoices.jsx
+++ src/pages/Invoices.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import btnImg from "../images/btnImg.svg";
 import data from "../assets/data.json";
+import btnImg from "../images/btnImg.svg";
 import novicesImg from "../images/novicesImg.svg";
 
 function Invoices() {
@@ -49,65 +49,72 @@ function Invoices() {
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-col justify-center gap-5 dark:text-white">
+                <div className="flex flex-col justify-center gap-2 dark:text-white">
                     {voices.length > 0 ? (
                         voices.map((value, index) => {
                             return (
                                 <div
-                                    className="max-w-[1000px] p-[24px] lg:flex justify-between shadow-md rounded-[8px] cursor-pointer hover:shadow-xl transition-all"
+                                    className="px-4 sm:px-6 mt-4 flex flex-col gap-[16px]"
                                     key={value.id}
                                 >
-                                    <div className="flex items-center justify-between">
-                                        <h1 className="text-[12px] font-bold">
-                                            <span className="text-[#7E88C3]">
-                                                #
-                                            </span>
-                                            {value.id}
-                                        </h1>
-                                        <h1 className="text-[#858BB2] text-[12px]">
-                                            {value.clientName}
-                                        </h1>
-                                    </div>
-                                    <div className="sm:flex xl:flex  sm:items-center sm:justify-between mt-[24px]">
-                                        <div className="">
-                                            <h1 className="text-[12px] text-[#888EB0] dark:text-[#DFE3FA]">
-                                                {value.paymentDue}
-                                            </h1>
-                                            <h1 className="text-[16px] text-[#0C0E16] font-bold dark:text-white">
-                                                £{value.total.toFixed(2)}
-                                            </h1>
+                                    <div
+                                        className="daerk:text-white shadow-md hover:shadow-xl dark:bg-[#1E2139] text-black drop-shadow-md bg-whitea
+                                         sm:w-full rounded-lg p-6 flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4 cursor-pointer hover:drop-shadow-lg transition-all"
+                                    >
+                                        <div className="flex items-center justify-between w-full sm:justify-start sm:gap-8 sm:w-auto">
+                                            <h2 className="text-sm font-extrabold dark:text-white">
+                                                <span className="text-[#7E88C3]">
+                                                    #
+                                                </span>
+                                                {value.id}
+                                            </h2>
+                                            <h2 className="sm:block dark:text-white">
+                                                {value.clientName}
+                                            </h2>
                                         </div>
-                                        <div
-                                            className={`flex items-center gap-[8px] py-[13px] px-[18px] rounded-[8px] ${
-                                                value.status === "paid"
-                                                    ? "bg-[#F3FCF9] dark:bg-[#1F2B3F]"
-                                                    : value.status === "draft"
-                                                    ? "bg-[#F3F3F5] dark:bg-[#292C44]"
-                                                    : "bg-[#FFF8F0] dark:bg-[#292C44]"
-                                            }`}
-                                        >
+                                        <div className="flex justify-between w-full sm:w-auto sm:justify-start sm:gap-8">
+                                            <div className="md:flex md:items-center md:gap-[73px]">
+                                                <p className=" text-[#888EB0] dark:text-[#DFE3FA] text-xs  xl:flex">
+                                                    {value.paymentDue}
+                                                </p>
+                                                <h2 className="text-lg font-bold text-[#0C0E16] dark:text-white">
+                                                    £{value.total.toFixed(2)}
+                                                </h2>
+                                            </div>
                                             <div
-                                                className={`w-[8px] h-[8px] rounded-full ${
-                                                    value.status === "paid"
-                                                        ? "bg-[#33D69F]"
-                                                        : value.status ===
-                                                          "draft"
-                                                        ? "bg-[#373B53]"
-                                                        : "bg-[#FF8F00]"
-                                                }`}
-                                            ></div>
-                                            <h1
-                                                className={
-                                                    value.status === "paid"
-                                                        ? "text-[#33D69F]"
-                                                        : value.status ===
-                                                          "draft"
-                                                        ? "text-[#373B53]"
-                                                        : "text-[#FF8F00]"
-                                                }
+                                                className={`
+                                                    ${
+                                                        value.status == "paid"
+                                                            ? "bg-[#F3FCF9] dark:bg-[#1F2B3F]"
+                                                            : value.status ==
+                                                              "draft"
+                                                            ? "bg-[#F3F3F5] dark:bg-[#292C44]"
+                                                            : "bg-[#FFF8F0] dark:bg-[#2B2736]"
+                                                    } flex items-center justify-center rounded-md gap-2 w-[104px] h-10`}
                                             >
-                                                {value.status}
-                                            </h1>
+                                                <div
+                                                    className={`w-2 h-2 rounded-full ${
+                                                        value.status == "paid"
+                                                            ? "bg-[#33D69F]"
+                                                            : value.status ==
+                                                              "draft"
+                                                            ? "bg-[#FFF8F0]"
+                                                            : " bg-[#FF8F00] "
+                                                    }`}
+                                                ></div>
+                                                <h2
+                                                    className={`${
+                                                        value.status == "paid"
+                                                            ? "text-[#33D69F]"
+                                                            : value.status ==
+                                                              "draft"
+                                                            ? "text-[#fff]"
+                                                            : "text-[#FF8F00]"
+                                                    } text-sm`}
+                                                >
+                                                    {value.status}
+                                                </h2>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
